6f239ebb059eda9e08a1899df05107ab4bba0c16
VanDuong279 <149557367+VanDuong279@users.noreply.github.com>
2024-10-13 10:39:18 +0700

    Delete IposProduct directory

--- IposProduct/src/main/java/codegym/vn/iposproduct/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package codegym.vn.iposproduct.repository;
-
-import codegym.vn.iposproduct.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-}
--- IposProduct/src/main/java/codegym/vn/iposproduct/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package codegym.vn.iposproduct.repository;
-
-import codegym.vn.iposproduct.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}
