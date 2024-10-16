ba81946add7aa12df020feea75ad2f43021dc1e0
ADMIN <lequangtoan12102000@gmail.com>
2024-10-16 20:26:15 +0700

    cap nhat homecontroller

--- C1023L1-BE/project-C1023I1/src/main/java/com/example/projectc1023i1/repository/product/ICategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectc1023i1.repository.product;
-
-import com.example.projectc1023i1.model.product.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category, Integer> {
-}
