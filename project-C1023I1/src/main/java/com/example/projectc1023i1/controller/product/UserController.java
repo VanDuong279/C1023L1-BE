cba7d764947ab0fd500e0a5265c0ae4bec2828bb
TranXuan2212 <tranduonghuuxuan22@gmail.com>
2024-10-17 10:52:07 +0700

    xuan da update

--- project-C1023I1/src/main/java/com/example/projectc1023i1/controller/product/UserController.java
+++ project-C1023I1/src/main/java/com/example/projectc1023i1/controller/product/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
