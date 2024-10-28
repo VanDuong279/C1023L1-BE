bb0a34d0e3bfc84f51aa9c4c683252ce842da5f9
hien1171 <xyzwn12345@gmail.com>
2024-10-28 18:25:21 +0700

    up

--- project-C1023I1/src/main/java/com/example/projectc1023i1/repository/product/TableRepository.java
+++ project-C1023I1/src/main/java/com/example/projectc1023i1/repository/product/TableRepository.java
@@ -2,6 +2,27 @@ package com.example.projectc1023i1.repository.product;
 
 import com.example.projectc1023i1.model.product.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TableRepository extends JpaRepository<Table,Integer> {
+    @Query(value = "select t.id as tableId, t.code as tableCode, t.status as tableStatus from Table as t")
+    List<Table> findAllTable();
+
+    @Query(value = "select t.id as tableId, t.code as tableCode, t.status as tableStatus from Table as t where t.code like :table_code ")
+    Table findTableByCode(@Param("table_code") String tableCode);
+
+    @Query(value = "select t.id as tableId, t.code as tableCode, t.status as tableStatus from Table as t where t.status = :status ")
+    List<Table> findTableByStatus(@Param("status") boolean status);
+
+
+
+    @Query(value = "DELETE FROM Table t WHERE t.id = :tableId")
+    boolean deleteTableById(@Param("tableId") int tableId);
+
+
+    @Query(value = "UPDATE Table t SET t.status = :newStatus WHERE t.id = :tableId")
+    void updateTableById(@Param("tableId") Long tableId, @Param("newStatus") boolean newStatus);
 }
--- project-C1023I1/src/main/java/com/example/projectc1023i1/service/product/TableService.java
+++ project-C1023I1/src/main/java/com/example/projectc1023i1/service/product/TableService.java
@@ -1,6 +1,7 @@
 package com.example.projectc1023i1.service.product;
 
 import com.example.projectc1023i1.model.product.Table;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +11,11 @@ public interface TableService {
     Table findById(int id);            // Tìm bàn theo ID
     void save(Table table);            // Lưu bàn mới hoặc cập nhật bàn
     void deleteById(int id);           // Xóa bàn theo ID
+
+    List<Table> findAllTable();
+    Table findTableByCode(@Param("table_code") String tableCode);
+    List<Table> findTableByStatus(@Param("status") boolean status);
+    boolean deleteTableById(@Param("tableId") int tableId);
+    void updateTableById(@Param("tableId") Long tableId, @Param("newStatus") boolean newStatus);
 }
 
